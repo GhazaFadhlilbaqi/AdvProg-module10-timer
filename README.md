6780ae7da01e48786fc38f845cde646c841b5b4d
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-05-17 18:06:47 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ## Understanding how it works
 ![](images/experiment1.png)
 The newly added message that was placed under the spawner is **printed first**, before the print statements inside the spawner above it. This happens because the first two prints are placed in an ```async``` function that is called by the spawner. The spawner is a process that is running in the background and does not halt the main process, so the main function can just proceed to the next instruction, which in this case, is printing the newly added message.
+
+## Multiple Spawn and removing drop
+![](images/experiment2.png)
+When the ```drop statement``` is removed, the program won't end after printing all the messages within the spawners. The executor basically waits for more tasks to run since ```drop``` is never called. So the ```drop``` function is basically a way to tell the executor that all of the tasks have been run, and that it can stop.
--- src/main.rs
+++ src/main.rs
@@ -104,11 +104,23 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Ghaza's Machine: done!");
     });
+        spawner.spawn(async {
+        println!("Ghaza's Machine: howdy numero dos!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Ghaza's Machine: done numero dos!");
+    });
+        spawner.spawn(async {
+        println!("Ghaza's Machine: howdy numero tres!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Ghaza's Machine: done numero tres!");
+    });
     println!("Ghaza's Machine: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    //drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
